56cdfdb63822f9617fc8d0b42a14a4ef668a02d3
niteshrajbaral <be2020se727@gces.edu.np>
2022-06-07 08:49:34 +0545

    extra added

---
+++ JavaScript/Lab1/external.js
@@ -0,0 +1,6 @@
+document.getElementById("click").addEventListener("click",function(){
+    alert("Clicking");
+});
+document.getElementById("hover").addEventListener("mouseover",function(){
+    alert("Hovering");
+});
